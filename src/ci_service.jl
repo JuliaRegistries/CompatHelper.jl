3da853e3bbd34a7172bbd1a6e2c37eafba82c45b
Dilum Aluthge <dilum@aluthge.com>
2019-11-04 00:04:01 -0500

    Update ci_service.jl

--- src/ci_service.jl
+++ src/ci_service.jl
@@ -1,5 +1,18 @@
+function github_actions_set_git_identity!()
+    try
+        run(`git config --global user.email 'nobody@example.com'`)
+    catch
+    end
+    try
+        run(`git config --global user.name 'Compat Helper'`)
+    catch
+    end
+    return nothing
+end
+
 function auto_detect_ci_service(; env=ENV)
     if haskey(env, "GITHUB_REPOSITORY")
+        github_actions_set_git_identity!()
         return GitHubActions()
     else
          error("Could not detect CI service")
