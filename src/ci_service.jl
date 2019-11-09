054db0ae9bdf6d8de8b51aaccc5a7528fadd5e89
Dilum Aluthge <dilum@aluthge.com>
2019-11-09 03:10:09 -0500

    Increase test coverage (#84)
    * Increase test coverage
    
    * Update ci_service.jl

--- src/ci_service.jl
+++ src/ci_service.jl
@@ -1,34 +1,14 @@
-function _git_user_name_is_set()::Bool
-    result = try
-        success(`git config user.name`)
-    catch
-        false
-    end
-    return result
+function GitHubActions()
+    return GitHubActions("github-actions[bot]")
 end
 
-function _git_user_email_is_set()::Bool
-    result = try
-        success(`git config user.name`)
-    catch
-        false
-    end
-    return result
+function get_my_username(ci_cfg::GitHubActions; auth = nothing, env::AbstractDict = ENV)
+    return ci_cfg.username
 end
 
 function set_git_identity(ci_cfg::GitHubActions)
-    if !_git_user_name_is_set()
-        try
-            run(`git config user.name 'github-actions[bot]'`)
-        catch
-        end
-    end
-    if !_git_user_email_is_set()
-        try
-            run(`git config user.email '41898282+github-actions[bot]@users.noreply.github.com'`)
-        catch
-        end
-    end
+    run(`git config user.name 'github-actions[bot]'`)
+    run(`git config user.email '41898282+github-actions[bot]@users.noreply.github.com'`)
     return nothing
 end
 
@@ -48,8 +28,4 @@ end
 function github_token(ci_cfg::GitHubActions; env::AbstractDict = ENV)
     result = env["GITHUB_TOKEN"]::String
     return result
-end
-
-function get_my_username(ci_cfg::GitHubActions; auth = nothing, env::AbstractDict = ENV)
-    return "github-actions[bot]"
 end
