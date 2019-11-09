5b13cf0299a4bf6ef0facb58890aa5da32a184a6
Dilum Aluthge <dilum@aluthge.com>
2019-11-09 01:31:45 -0500

    Progress commit

--- test/integration-tests.jl
+++ test/integration-tests.jl
@@ -8,8 +8,9 @@ repo = GitHub.repo(COMPATHELPER_INTEGRATION_TEST_REPO; auth = auth)
 Test.@test success(`git --version`)
 @info("Authenticated to GitHub as \"$(whoami)\"")
 
-close_all_pull_requests(repo; auth = auth, state = "open")
 delete_stale_branches(repo_url_with_auth)
+# close_all_pull_requests(repo; auth = auth, state = "open")
+# delete_stale_branches(repo_url_with_auth)
 
 with_master_branch(templates("master_1"), "master"; repo_url = repo_url_with_auth) do master_1
     withenv("GITHUB_REPOSITORY" => COMPATHELPER_INTEGRATION_TEST_REPO, "GITHUB_TOKEN" => TEST_USER_GITHUB_TOKEN) do
