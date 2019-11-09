f73dced16800e6c819349080af5ddeb118268465
Dilum Aluthge <dilum@aluthge.com>
2019-11-08 22:28:39 -0500

    Update integration-tests.jl

--- test/integration-tests.jl
+++ test/integration-tests.jl
@@ -3,7 +3,7 @@ include("utils.jl")
 COMPATHELPER_INTEGRATION_TEST_REPO = ENV["COMPATHELPER_INTEGRATION_TEST_REPO"]::String
 TEST_USER_GITHUB_TOKEN = ENV["BCBI_TEST_USER_GITHUB_TOKEN"]::String
 auth = GitHub.authenticate(TEST_USER_GITHUB_TOKEN)
-whoami = RegistryCI.AutoMerge.username(auth)
+whoami = username(auth)
 repo_url_without_auth = "https://github.com/$(AUTOMERGE_INTEGRATION_TEST_REPO)"
 repo_url_with_auth = "https://$(whoami):$(TEST_USER_GITHUB_TOKEN)@github.com/$(AUTOMERGE_INTEGRATION_TEST_REPO)"
 repo = GitHub.repo(AUTOMERGE_INTEGRATION_TEST_REPO; auth = auth)
