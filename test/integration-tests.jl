fa4090de17b8268d4ab0f195766292246aca1604
Dilum Aluthge <dilum@aluthge.com>
2019-11-08 22:41:35 -0500

    Update integration-tests.jl

--- test/integration-tests.jl
+++ test/integration-tests.jl
@@ -4,9 +4,9 @@ COMPATHELPER_INTEGRATION_TEST_REPO = ENV["COMPATHELPER_INTEGRATION_TEST_REPO"]::
 TEST_USER_GITHUB_TOKEN = ENV["BCBI_TEST_USER_GITHUB_TOKEN"]::String
 auth = GitHub.authenticate(TEST_USER_GITHUB_TOKEN)
 whoami = username(auth)
-repo_url_without_auth = "https://github.com/$(AUTOMERGE_INTEGRATION_TEST_REPO)"
-repo_url_with_auth = "https://$(whoami):$(TEST_USER_GITHUB_TOKEN)@github.com/$(AUTOMERGE_INTEGRATION_TEST_REPO)"
-repo = GitHub.repo(AUTOMERGE_INTEGRATION_TEST_REPO; auth = auth)
+repo_url_without_auth = "https://github.com/$(COMPATHELPER_INTEGRATION_TEST_REPO)"
+repo_url_with_auth = "https://$(whoami):$(TEST_USER_GITHUB_TOKEN)@github.com/$(COMPATHELPER_INTEGRATION_TEST_REPO)"
+repo = GitHub.repo(COMPATHELPER_INTEGRATION_TEST_REPO; auth = auth)
 @test success(`$(GIT) --version`)
 @info("Authenticated to GitHub as \"$(whoami)\"")
 
