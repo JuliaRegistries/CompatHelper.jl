73ee3b8fb89f6eeb28dbffadc70f982206ca0609
Dilum Aluthge <dilum@aluthge.com>
2020-07-10 17:19:29 +0000

    Update ci_service.jl

--- src/ci_service.jl
+++ src/ci_service.jl
@@ -1,22 +1,27 @@
 function GitHubActions()
-    return GitHubActions("github-actions[bot]")
+    result = GitHubActions(
+        "github-actions[bot]",
+        "41898282+github-actions[bot]@users.noreply.github.com",
+    )
+    return result
 end
 
 function TeamCity()
-    return TeamCity("github-actions[bot]", "41898282+github-actions[bot]@users.noreply.github.com")
+    result = TeamCity(
+        "github-actions[bot]",
+        "41898282+github-actions[bot]@users.noreply.github.com",
+    )
+    return result
 end
 
-function get_my_username(ci_cfg::Union{GitHubActions, TeamCity}; auth = nothing, env::AbstractDict = ENV)
+function get_my_username(ci_cfg::Union{GitHubActions, TeamCity};
+                         auth = nothing,
+                         env::AbstractDict = ENV)
     return ci_cfg.username
 end
 
-function set_git_identity(ci_cfg::GitHubActions)
-    run(`git config user.name 'github-actions[bot]'`)
-    run(`git config user.email '41898282+github-actions[bot]@users.noreply.github.com'`)
-    return nothing
-end
 
-function set_git_identity(ci_cfg::TeamCity)
+function set_git_identity(ci_cfg::Union{GitHubActions, TeamCity})
     run(`git config user.name '$(ci_cfg.username)'`)
     run(`git config user.email '$(ci_cfg.email)'`)
     return nothing
@@ -30,12 +35,14 @@ function auto_detect_ci_service(; env::AbstractDict = ENV)
     end
 end
 
-function github_repository(ci_cfg::Union{GitHubActions, TeamCity}; env::AbstractDict = ENV)
+function github_repository(ci_cfg::Union{GitHubActions, TeamCity};
+                           env::AbstractDict = ENV)
     result = env["GITHUB_REPOSITORY"]::String
     return result
 end
 
-function github_token(ci_cfg::Union{GitHubActions, TeamCity}; env::AbstractDict = ENV)
+function github_token(ci_cfg::Union{GitHubActions, TeamCity};
+                      env::AbstractDict = ENV)
     result = env["GITHUB_TOKEN"]::String
     return result
 end
