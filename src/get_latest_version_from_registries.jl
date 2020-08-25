2ceec581e3112c351bc38832b6441e8979257386
Matěj Račinský <matej.racinsky@avast.com>
2020-08-25 10:10:25 +0200

    making usage of pkg server optional

--- src/get_latest_version_from_registries.jl
+++ src/get_latest_version_from_registries.jl
@@ -1,8 +1,24 @@
 import Pkg
 import UUIDs
 
+function git_clone(tmp_dir, previous_director, url, name)
+    cd(tmp_dir)
+    run(`git clone $(url) $(name)`)
+    cd(previous_directory)
+end
+
+function download_or_clone(tmp_dir, previous_director, reg_url, registry_path, url, name)
+    try
+        Pkg.Types.download_verify_unpack(reg_url, nothing, registry_path, ignore_existence = true)
+    catch err
+        # in case of fail, fallback to clone
+        git_clone(tmp_dir, previous_director, url, name)
+    end
+end
+
 function get_latest_version_from_registries!(dep_to_latest_version::Dict{Package, Union{VersionNumber, Nothing}},
-                                             registry_list::Vector{Pkg.Types.RegistrySpec})
+                                             registry_list::Vector{Pkg.Types.RegistrySpec};
+                                             use_pkg_server::Book=false)
     original_directory = pwd()
     num_registries = length(registry_list)
     registry_temp_dirs = Vector{String}(undef, num_registries)
@@ -14,44 +30,33 @@ function get_latest_version_from_registries!(dep_to_latest_version::Dict{Package
         name = registry.name
         url = registry.url
         uuid = registry.uuid
-        registry_path = joinpath(tmp_dir, name)
-        @static if VERSION >= v"1.5.0"
-            # copied from https://github.com/JuliaLang/Pkg.jl/blob/release-1.5/src/Types.jl#L981
-            reg_url, registry_urls = pkg_server_registry_url(reg.uuid, registry_urls)
-            if reg_url !== nothing
-                try
-                    Pkg.Types.download_verify_unpack(reg_url, nothing, registry_path, ignore_existence = true)
-                    @info("Downloaded registry $reg_url from package server.")
-                catch err
-                    nothing
+        previous_directory = pwd()
+        if use_pkg_server
+            registry_path = joinpath(tmp_dir, name)
+            @static if VERSION >= v"1.5.0"
+                # copied from https://github.com/JuliaLang/Pkg.jl/blob/release-1.5/src/Types.jl#L981
+                reg_url, registry_urls = pkg_server_registry_url(uuid, registry_urls)
+                if reg_url !== nothing
+                    download_or_clone(tmp_dir, previous_director, reg_url, registry_path, url, name)
+                elseif url !== nothing # clone from url
+                    git_clone(tmp_dir, previous_director, url, name)
+                else
+                    error("Could not download registry, nor pkg server not git download works.")
                 end
-            elseif url !== nothing # clone from url
-                cd(tmp_dir)
-                run(`git clone $(url) $(name)`)
-                cd(previous_directory)
-            end
-        elseif v"1.4.0" <= VERSION < v"1.5.0"
-            # copied from https://github.com/JuliaLang/Pkg.jl/blob/v1.4.2/src/Types.jl#L921
-            if (reg_url = Pkg.Types.pkg_server_registry_url(uuid)) !== nothing
-                # download from Pkg server
-                try
-                    Pkg.Types.download_verify_unpack(reg_url, nothing, registry_path, ignore_existence = true)
-                    @info("Downloaded registry $reg_url from package server.")
-                catch err
-                    nothing
+            elseif v"1.4.0" <= VERSION < v"1.5.0"
+                # copied from https://github.com/JuliaLang/Pkg.jl/blob/v1.4.2/src/Types.jl#L921
+                if (reg_url = Pkg.Types.pkg_server_registry_url(uuid)) !== nothing
+                    download_or_clone(tmp_dir, previous_director, reg_url, registry_path, url, name)
+                elseif url !== nothing # clone from url
+                    git_clone(tmp_dir, previous_director, url, name)
+                else
+                    error("Could not download registry, nor pkg server not git download works.")
                 end
-            elseif url !== nothing # clone from url
-                cd(tmp_dir)
-                run(`git clone $(url) $(name)`)
-                cd(previous_directory)
             else
-                error("Could not download registry, nor pkg server not git download works.")
+                git_clone(tmp_dir, previous_director, url, name)
             end
         else
-            # original way of obtaining registry
-            cd(tmp_dir)
-            run(`git clone $(url) $(name)`)
-            cd(previous_directory)
+            git_clone(tmp_dir, previous_director, url, name)
         end
     end
     for (registry_temp_dir, registry) in zip(registry_temp_dirs, registry_list)
