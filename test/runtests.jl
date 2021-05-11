4f3810d0f56a2767ce2b4cba595ff96a7e311744
Dilum Aluthge <dilum@aluthge.com>
2021-05-11 19:10:52 -0400

    Add a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using CompatHelper
 using Test
 
 @testset "CompatHelper.jl" begin
-
+    @test CompatHelper.hello(1) == 2 # TODO: delete this line once we have deleted the `CompatHelper.hello` function
 end
