99e82146ccdf7497f221a0f1c30f000761bfe2ae
Dilum Aluthge <dilum@aluthge.com>
2021-05-11 19:09:57 -0400

    Add a dummy source code line

--- src/CompatHelper.jl
+++ src/CompatHelper.jl
@@ -1,3 +1,7 @@
 module CompatHelper
 
-end
+# TODO: delete this line. It is just here because you can't have 100% code coverage on an empty module.
+# Once we have some actual source code, we will delete this line.
+hello(x) = x + x
+
+end # module
