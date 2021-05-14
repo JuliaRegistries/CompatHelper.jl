85851fa694e089412b6f1ff9eb92726895129033
Matt Brzezinski <matt.brzezinski@invenia.ca>
2021-05-14 17:57:48 -0500

    Addressed #304

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,24 @@ using GitForge
 using GitForge: GitHub, GitLab
 using Mocking
 using Test
+using TOML
 
 
 Mocking.activate()
 
 include("patches.jl")
+
+@testset "`version =` line in the workflow file" begin
+    root_directory = dirname(dirname(@__FILE__))
+    project_file = joinpath(root_directory, "Project.toml")
+    version = Base.VersionNumber(TOML.parsefile(project_file)["version"])
+    major_version = version.major
+    @test major_version >= 1
+    workflow_dir = joinpath(root_directory, ".github", "workflows")
+    workflow_filename = joinpath(workflow_dir, "CompatHelper.yml")
+    workflow_filecontents = read(workflow_filename, String)
+    @test occursin(Regex("\\sversion = \"$(major_version)\"\n"), workflow_filecontents)
+end
 
 @testset "CompatHelper.jl" begin
     include("pull_requests.jl")
