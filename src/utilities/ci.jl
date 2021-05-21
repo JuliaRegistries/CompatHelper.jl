bd5ef68c4973972027745cedee13205f93840b7a
Matt Brzezinski <matt.brzezinski@invenia.ca>
2021-05-21 14:28:32 -0500

    Removed setting GitConfig

--- src/utilities/ci.jl
+++ src/utilities/ci.jl
@@ -9,13 +9,6 @@ GitHubActions() = GitHubActions("github-actions[bot]", "41898282+github-actions[
 github_repository(env::AbstractDict=ENV) = env["GITHUB_REPOSITORY"]
 github_token(env::AbstractDict=ENV) = env["GITHUB_TOKEN"]
 
-function set_git_identity(ci_cfg::GitHubActions)
-    run(Cmd(String["git", "config", "user.name", strip(ci_cfg.username)]))
-    run(Cmd(String["git", "config", "user.email", strip(ci_cfg.email)]))
-
-    return nothing
-end
-
 function auto_detect_ci_service(; env::AbstractDict=ENV)
     if haskey(env, "GITHUB_REPOSITORY")
         return GitHubActions()
