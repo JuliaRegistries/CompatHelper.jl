5e5181be2d10d9c992c5c3d14c296b6c489e3bc2
Matt Brzezinski <matt.brzezinski@invenia.ca>
2021-05-21 11:13:18 -0500

    Added set_git_identity tests

--- test/utilities/ci.jl
+++ test/utilities/ci.jl
@@ -18,3 +18,30 @@
         end
     end
 end
+
+@testset "set_git_identity" begin
+    function read_git_config()
+        try
+            name = strip(read(`git config user.name`, String))
+            email = strip(read(`git config user.email`, String))
+
+            return name, email
+        catch
+            return "", ""
+        end
+    end
+
+    original = CompatHelper.GitHubActions(read_git_config()...)
+    config = CompatHelper.GitHubActions()
+
+    try
+        CompatHelper.set_git_identity(config)
+
+        name, email = read_git_config()
+
+        @test name == config.username
+        @test email == config.email
+    finally
+        CompatHelper.set_git_identity(original)
+    end
+end
