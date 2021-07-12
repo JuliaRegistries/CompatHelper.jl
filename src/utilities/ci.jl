8a5b45347c52b44e090b2dc7ab357f73ca7917ed
Fernando Chorney <fernando.chorney@invenia.ca>
2021-07-12 15:12:00 -0500

    Put constructors in struct

--- src/utilities/ci.jl
+++ src/utilities/ci.jl
@@ -3,30 +3,36 @@ abstract type CIService end
 struct GitHubActions <: CIService
     username::String
     email::String
+
+    function GitHubActions(
+        username="github-actions[bot]",
+        email="41898282+github-actions[bot]@users.noreply.github.com",
+    )
+        return new(username, email)
+    end
 end
 
 struct GitLabCI <: CIService
     username::String
     email::String
-end
 
-function GitHubActions()
-    return GitHubActions(
-        "github-actions[bot]", "41898282+github-actions[bot]@users.noreply.github.com"
+    function GitLabCI(
+        username="gitlab-ci[bot]",
+        email="gitlab-ci[bot]@gitlab.com",
     )
-end
-
-function GitLabCI()
-    return GitLabCI("gitlab-ci[bot]", "gitlab-ci[bot]@gitlab.com")
+        return new(username, email)
+    end
 end
 
 api_hostname(::GitHubActions) = "https://api.github.com"
 api_hostname(::GitLabCI) = "https://gitlab.com/api/v4"
+
 clone_hostname(::GitHubActions) = "github.com"
 clone_hostname(::GitLabCI) = "gitlab.com"
 
 ci_repository(::GitHubActions, env::AbstractDict=ENV) = env["GITHUB_REPOSITORY"]
 ci_repository(::GitLabCI, env::AbstractDict=ENV) = env["CI_PROJECT_PATH"]
+
 ci_token(::GitHubActions, env::AbstractDict=ENV) = env["GITHUB_TOKEN"]
 ci_token(::GitLabCI, env::AbstractDict=ENV) = env["GITLAB_TOKEN"]
 
