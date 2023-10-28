8c7b462ac3c40cc7a4984f5d1896ba077fb9d93b
Seth Axen <seth@sethaxen.com>
2023-10-28 13:16:05 +0200

    Increase count for pr length check

--- test/main.jl
+++ test/main.jl
@@ -62,7 +62,7 @@
                     "GITHUB_REPOSITORY" => "CompatHelper.jl", "GITHUB_TOKEN" => "token"
                 ) do
                     prs = CompatHelper.main()
-                    @test length(prs) == 2
+                    @test length(prs) == 3
                     @test prs[1] isa GitHub.PullRequest
                     @test prs[2] isa GitHub.PullRequest
                 end
@@ -97,7 +97,7 @@
                     ) do
                         delete!(ENV, "GITHUB_REPOSITORY")
                         prs = CompatHelper.main()
-                        @test length(prs) == 2
+                        @test length(prs) == 3
                         @test prs[1] isa GitLab.MergeRequest
                         @test prs[2] isa GitLab.MergeRequest
                     end
